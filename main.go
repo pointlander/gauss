33100b466b03e1aa9ccdce98382b4df1695509ba
Andrew Snodgrass <pointlander@gmail.com>
2024-03-24 22:16:20 -0500

    Compute inverse

--- main.go
+++ main.go
@@ -11,6 +11,29 @@ import (
 	. "github.com/pointlander/matrix"
 )
 
+func MatrixInverse(m Matrix, rng *Rand) (ai Matrix) {
+	identity := NewIdentityMatrix(m.Cols)
+	s := Meta(512, 2.7e-1, .1, rng, 4, .1, 1, false, func(samples []Sample, x ...Matrix) {
+		done := make(chan bool, 8)
+		process := func(index int) {
+			x := samples[index].Vars[0][0].Sample()
+			y := samples[index].Vars[0][1].Sample()
+			z := samples[index].Vars[0][2].Sample()
+			ai := x.Add(y.H(z))
+			cost := m.MulT(ai).Quadratic(identity).Avg()
+			samples[index].Cost = float64(cost.Data[0])
+			done <- true
+		}
+		for j := range samples {
+			go process(j)
+		}
+		for range samples {
+			<-done
+		}
+	}, m)
+	return s.Vars[0][0].Sample().Add(s.Vars[0][1].Sample().H(s.Vars[0][2].Sample()))
+}
+
 func main() {
 	rng := Rand(1)
 	datum, err := iris.Load()
@@ -27,4 +50,7 @@ func main() {
 	multi.LearnA(&rng, nil)
 	fmt.Println(multi.E)
 	fmt.Println(multi.A)
+	inv := MatrixInverse(multi.A, &rng)
+	fmt.Println(inv)
+	fmt.Println(multi.A.MulT(inv))
 }
