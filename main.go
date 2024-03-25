c52e2a322d7f10d56d99c752e5221f1480401e03
Andrew Snodgrass <pointlander@gmail.com>
2024-03-24 20:47:24 -0500

    Calculate multivariate distribution of iris dataset

--- main.go
+++ main.go
@@ -4,6 +4,27 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/pointlander/datum/iris"
+	. "github.com/pointlander/matrix"
+)
+
+func main() {
+	rng := Rand(1)
+	datum, err := iris.Load()
+	if err != nil {
+		panic(err)
+	}
+	points := NewMatrix(4, 150)
+	for _, row := range datum.Fisher {
+		for _, value := range row.Measures {
+			points.Data = append(points.Data, float32(value))
+		}
+	}
+	multi := NewMultiFromData(points.T())
+	multi.LearnA(&rng, nil)
+	fmt.Println(multi.E)
+	fmt.Println(multi.A)
 }
