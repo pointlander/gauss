06cc000926c0fec56df924184d16fe59b60375c5
Andrew Snodgrass <pointlander@gmail.com>
2024-03-25 17:50:18 -0500

    Added variance reduction maximization

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/pointlander/datum/iris"
 	. "github.com/pointlander/matrix"
@@ -35,7 +36,7 @@ func MatrixInverse(m Matrix, rng *Rand) (ai Matrix) {
 }
 
 func main() {
-	rng := Rand(1)
+	//rng := Rand(1)
 	datum, err := iris.Load()
 	if err != nil {
 		panic(err)
@@ -47,12 +48,66 @@ func main() {
 		}
 	}
 	multi := NewMultiFromData(points.T())
-	multi.LearnA(&rng, nil)
 	fmt.Println(multi.E)
+	sort.Slice(datum.Fisher, func(i, j int) bool {
+		return datum.Fisher[i].Measures[0] < datum.Fisher[j].Measures[0]
+	})
+	in := float32(0.0)
+	for i := 0; i < 4; i++ {
+		x := multi.E.Data[i]
+		if x < 0 {
+			x = -x
+		}
+		in += x
+	}
+	max, index := float32(0.0), 0
+	for i := 0; i < len(datum.Fisher)-1; i++ {
+		a := NewMatrix(4, i+1)
+		b := NewMatrix(4, len(datum.Fisher)-(i+1))
+		for j := 0; j < i+1; j++ {
+			row := datum.Fisher[j]
+			for _, value := range row.Measures {
+				a.Data = append(a.Data, float32(value))
+			}
+		}
+		for j := i + 1; j < len(datum.Fisher); j++ {
+			row := datum.Fisher[j]
+			for _, value := range row.Measures {
+				b.Data = append(b.Data, float32(value))
+			}
+		}
+		aa := NewMultiFromData(a.T())
+		aaa := float32(0.0)
+		for i := 0; i < 4; i++ {
+			x := aa.E.Data[i]
+			if x < 0 {
+				x = -x
+			}
+			aaa += x
+		}
+		bb := NewMultiFromData(b.T())
+		bbb := float32(0.0)
+		for i := 0; i < 4; i++ {
+			x := bb.E.Data[i]
+			if x < 0 {
+				x = -x
+			}
+			bbb += x
+		}
+		if gap := in - (aaa + bbb); gap > max {
+			fmt.Println(i, in, aaa, bbb, gap)
+			max, index = gap, i
+		}
+	}
+	fmt.Println(index)
+	for i := 0; i < index; i++ {
+		fmt.Println(datum.Fisher[i].Label)
+	}
+	/*multi.LearnA(&rng, nil)
 	fmt.Println(multi.A)
 	inv := MatrixInverse(multi.A, &rng)
 	fmt.Println(inv)
 	fmt.Println(multi.A.MulT(inv))
 	dist := inv.MulT(points.Sub(multi.U))
-	fmt.Println(dist)
+	fmt.Println(dist)*/
 }
